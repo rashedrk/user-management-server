e4100dbf7802293d40b0027625d0eafd88696a16
rashedrk <rashedrk001@gmail.com>
2023-05-13 00:29:36 +0600

    Create React form and Post API and send data to the server

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const users = [
+    {id:1, name: "sabrina", email: "sabrina@gmail.com"},
+    {id:2, name: "samira", email: "samira@gmail.com"},
+    {id:3, name: "laboni", email: "laboni@gmail.com"}
+]
+
+app.get('/', (req,res) => {
+    res.send("user management server is running.");
+})
+
+app.get('/users', (req,res) => {
+    res.send(users);
+})
+
+app.post('/users', (req,res) => {
+    console.log('post is hitting');
+    console.log(req.body);
+})
+app.listen(port , () => {
+    console.log(`server is running at : ${port}`);
+})
